47cbf4919b0997fdba5554a08c063318ed431376
AntoineDupre <antoine.dupre@maxiv.lu.se>
2019-08-26 13:31:13 +0200

    Integrate docker commands

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ extern crate reqwest;
 extern crate skim;
 use skim::{Skim, SkimOptionsBuilder};
 use std::collections::HashMap;
+use std::env;
 use std::io::Cursor;
+use std::process::Command;
 
 fn main() -> Result<(), Box<std::error::Error>> {
+    // Get args
+    let args: Vec<String> = env::args().collect();
+    let mut query = "";
+    if args.len() > 1 {
+        query = &args[1];
+    }
+
     // Http query on the docker registry
     let resp_json: HashMap<String, Vec<String>> =
         reqwest::get("https://docker.maxiv.lu.se/v2/_catalog")?.json()?;
@@ -35,8 +44,25 @@ fn main() -> Result<(), Box<std::error::Error>> {
     let mut image = String::from("docker.maxiv.lu.se/");
     for item in selected_items.iter() {
         image.push_str(item.get_output_text().to_string().trim());
-        // Print in stdout the result
-        print!("{}", image);
+    }
+
+    // Run docker in interactive mode
+    if query == "run" {
+        Command::new("docker")
+            .arg("run")
+            .arg("-i")
+            .arg("-t")
+            .arg(&image)
+            .status()
+            .expect("failed to execute process");
+
+    // Only pull the container
+    } else {
+        Command::new("docker")
+            .arg("pull")
+            .arg(&image)
+            .status()
+            .expect("failed to execute process");
     }
 
     Ok(())
